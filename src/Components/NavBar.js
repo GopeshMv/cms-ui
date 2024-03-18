ae230ab96ca4e983ef9943c65e827222995f41c5
GopeshMv <80823826+GopeshMv@users.noreply.github.com>
2024-03-18 10:33:33 +0530

    Initial Commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReactDOM } from "react";
+import './NavBar.css';
+
+function NavBar () {
+
+    return (
+        <div className="TopNav">
+            <p className="fontNav">WELCOME BACK!</p>
+        </div>
+    );
+}
+
+export default NavBar;
