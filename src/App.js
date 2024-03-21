f896fa6b85a0e6e2ae701199621f8d56c6a6ead4
SDORAIS2 <113187196+SurajDs-git@users.noreply.github.com>
2024-03-21 21:23:47 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -6,19 +6,28 @@ import Home from './Components/Home/Home';
 import Payment from './Components/Payment/Payment';
 import Transactions from './Components/Transactions/Transactions';
 import Account from './Components/Account/Account';
+import Login from './Components/Login/Login';
 
 
 function App() {
     return (
         <div className="App">
+            <div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+            {/* <div>
             <SideNav />
             <div className="Main">
                 <TopNav />
                 <div className="Body">
                     <BrowserRouter>
                         <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route index element={<Home />} />
+
+                            <Route path="/home" element={<Home />} />
                             <Route path="/account" element={<Account />} />
                             <Route path="/payment" element={<Payment />} />
                             <Route path="/transactions" element={<Transactions />} />
@@ -26,7 +35,7 @@ function App() {
                     </BrowserRouter>
                 </div>
             </div>
-            
+            </div> */}
         </div>       
         );
     }
