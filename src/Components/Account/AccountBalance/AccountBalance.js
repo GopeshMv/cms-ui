f7129393191b79dd8af726752ae3be5205e07294
Aravintakshan A G <aravintakshan03cse@gmail.com>
2024-03-24 03:05:49 +0530

    Account_page

--- src/Components/Account/AccountBalance/AccountBalance.js
+++ src/Components/Account/AccountBalance/AccountBalance.js
@@ -6,8 +6,32 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function AccountBalance() {
+    const [DetailList, setDetailList] = useState({});
+
+    useEffect(() => {
+        const fetchDetail = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8090/account/1`);
+                console.log('Response:', response); // Log the full response
+                const name = response.data.user.name;
+                const balance = response.data.balance;
+                const openDate = response.data.openDate;
+                const password = response.data.password;
+                const type = response.data.user.type;
+                console.log(name);
+                setDetailList({name:name,balance:balance,openDate:openDate,password:password,type:type})
+               
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
+
+        fetchDetail();
+    }, []);
     return (
         <>
             <Card sx={{ minWidth: 530, minHeight: 400, margin: 1, backgroundColor: "#E1E2F7" }}>
@@ -16,7 +40,7 @@ function AccountBalance() {
                         Balance:
                     </Typography>
                     <Typography variant="body1" component="h1" sx={{margin:2, marginLeft:10,fontSize:"20px"}}>
-                        <CurrencyRupeeIcon sx={{fontSize:"17px"}}/>1000.00
+                        <CurrencyRupeeIcon sx={{fontSize:"17px"}}/>{DetailList.balance}
                     </Typography>
                     <Typography variant="body" component="h1">
                         Last Payment:
--- src/Components/Account/AccountDetail/AccountDetails.js
+++ src/Components/Account/AccountDetail/AccountDetails.js
@@ -8,6 +8,8 @@ import Typography from '@mui/material/Typography';
 import Popover from '@mui/material/Popover';
 import TextField from '@mui/material/TextField';
 import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
 function AccountDetails() {
@@ -23,6 +25,30 @@ function AccountDetails() {
 
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
+
+    const [DetailList, setDetailList] = useState({});
+
+    useEffect(() => {
+        const fetchDetail = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8090/account/1`);
+                console.log('Response:', response); // Log the full response
+                const name = response.data.user.name;
+                const balance = response.data.balance;
+                const openDate = response.data.openDate;
+                const password = response.data.password;
+                const type = response.data.user.type;
+                console.log(name);
+                setDetailList({name:name,balance:balance,openDate:openDate,password:password,type:type})
+               
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
+
+        fetchDetail();
+    }, []);
+
     return (
         <>
             <Card sx={{ minWidth: 530, minHeight: 400, margin: 1, backgroundColor: "#E1E2F7" }}>
@@ -31,7 +57,7 @@ function AccountDetails() {
                         Open Date:
                     </Typography>
                     <Typography variant="body1" component="h1" sx={{ margin: 3, marginLeft: 10, fontSize: "25px" }}>
-                        25Feb,2524
+                        {DetailList.openDate}
                     </Typography>
                     <Typography variant="body" component="h1">
                         Account Settings:
