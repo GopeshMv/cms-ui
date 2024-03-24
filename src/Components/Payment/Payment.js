24a30dfc0fe0236c9d7c164e5ab0135e8c00a6f2
JJTITUS <65460+JJTITUS@users.noreply.github.ford.com>
2024-03-24 19:32:17 +0530

    Added Payment Request Form for merchant

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 import BillingForm from "./BillingForm/BillingForm";
 import PaymentForm from "./PaymentForm/PaymentForm";
 import PaymentStatus from "./PaymentStatus/PaymentStatus";
+import PaymentRequestForm from "./PaymentRequestForm/PaymentRequestForm";
 
 function Payment() {
     const [billingCompleted, setBillingCompleted] = useState(false);
     const [paymentCompleted, setPaymentCompleted] = useState(false);
-    
+    const [requestCompleted, setRequestCompleted] = useState(false);
+
+    let user = "customer" ; // customer or merchant
+
     return (
         <>
-            { !paymentCompleted ?
-                (
-                    !billingCompleted ?
-                    <BillingForm billingCompleted={setBillingCompleted}/> :
-                    <PaymentForm billingCompleted={setBillingCompleted} paymentCompleted={setPaymentCompleted}/> 
+            { user == "customer" ?
+                ( !paymentCompleted ?
+                    (
+                        !billingCompleted ?
+                        <BillingForm billingCompleted={setBillingCompleted}/> :
+                        <PaymentForm billingCompleted={setBillingCompleted} paymentCompleted={setPaymentCompleted}/> 
+                    ) :
+                    <PaymentStatus />
                 ) :
-                <PaymentStatus />
+                <PaymentRequestForm requestCompleted={setRequestCompleted} />
+    
             }
         </>
     );
