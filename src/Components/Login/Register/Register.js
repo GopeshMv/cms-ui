96abdef4910e37ad6d5b0c893cf28a8ce9174df9
BGUNACHI <bhomegagk2002@gmail.com>
2024-03-24 23:09:38 +0530

    Add toggle button

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -1,7 +1,14 @@
 import React from "react";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import "./Register.css";
 
 function Register({ handleLoginToggle, isRegisterActive }) {
+    const [alignment, setAlignment] = React.useState('web');
+
+    const handleChange = (event, newAlignment) => {
+        setAlignment(newAlignment);
+    };
     return (
         <form className="RegisterForm">
             <fieldset disabled={!isRegisterActive} className="registerFieldset" >
@@ -10,11 +17,24 @@ function Register({ handleLoginToggle, isRegisterActive }) {
                     <span className="FormDetail">Please enter your details</span>
                 </div>
                 <div className="BillingBody">
+                    <div >
+                        <span className="BillingLabel">Registering as </span>
+                        <ToggleButtonGroup className="Paynow"
+                            color="primary"
+                            value={alignment}
+                            exclusive
+                            onChange={handleChange}
+                            aria-label="Platform"
+                        >
+                            <ToggleButton value="Customer">Customer</ToggleButton>
+                            <ToggleButton value="Merchant">Merchant</ToggleButton>
+                        </ToggleButtonGroup>
+                    </div>
                     <div className="CardChoice"></div>
                     <div className="CardSlideshow"></div>
                     <div className="CardDetails">
                         <label for="email" className="BillingLabel">Email ID</label>
-                        <input name="email" className="BillingTextBox" type="email"required></input>
+                        <input name="email" className="BillingTextBox" type="email" required></input>
                         <label for="name" classph noName="BillingLabel">Name</label>
                         <input name="name" className="BillingTextBox" required></input>
                         <label for="phone no" className="BillingLabel">Ph No</label>
@@ -24,7 +44,7 @@ function Register({ handleLoginToggle, isRegisterActive }) {
                         <label for="password" className="BillingLabel">Password</label>
                         <input name="password" className="BillingTextBox" type="password" required></input>
                         <label for="confirm password" className="BillingLabel">Confirm Password</label>
-                        <input name="confirm password" className="BillingTextBox" type="password"required></input>
+                        <input name="confirm password" className="BillingTextBox" type="password" required></input>
                         <p>Existing User? <button onClick={handleLoginToggle} className="smolLogin">Login</button></p>
                         <button onClick={handleLoginToggle} className="SignIn">Register</button>
 
--- src/Components/Login/SignIn/Login.js
+++ src/Components/Login/SignIn/Login.js
@@ -1,24 +1,47 @@
 import React, { useState } from "react";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import "./Login.css";
 
 
 function Login({ handleLoginToggle, isLoginActive }) {
     const [email, setEmail] = useState("");
+    const [alignment, setAlignment] = React.useState('web');
+
+    const handleChange = (event, newAlignment) => {
+        setAlignment(newAlignment);
+    };
     return (
         <form className="LoginForm">
             <fieldset disabled={!isLoginActive} className="loginFieldset" >
                 <div className="BillingFormHeader">
                     <span className="FormTitle">Login</span>
                     <span className="FormDetail">Please enter your credentials</span>
+
                 </div>
                 <div className="BillingBody">
+                    <div>
+                        <span className="BillingLabel">Logging in as </span>
+                        <ToggleButtonGroup className="Paynow"
+                            color="primary"
+                            value={alignment}
+                            exclusive
+                            onChange={handleChange}
+                            aria-label="Platform"
+                        >
+                            <ToggleButton value="Admin">Admin</ToggleButton>
+                            <ToggleButton value="User">User</ToggleButton>
+                        </ToggleButtonGroup>
+                    </div>
+
                     <div className="CardChoice"></div>
                     <div className="CardSlideshow"></div>
                     <div className="CardDetails">
                         <label for="email" className="BillingLabel">Email ID</label>
-                        <input name="email" className="BillingTextBox" required type = "email"></input>
+                        <input name="email" className="BillingTextBox" required type="email"></input>
                         <label for="password" className="BillingLabel">Password</label>
-                        <input name="password" className="BillingTextBox" required type = "password"></input>
+                        <input name="password" className="BillingTextBox" required type="password"></input>
+
                         <p>Not an Existing User? <button onClick={handleLoginToggle} className="smolRegister">Register</button></p>
                         <button className="SignIn">Sign In</button>
                     </div>
