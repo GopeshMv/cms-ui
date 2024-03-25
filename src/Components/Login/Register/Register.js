20446cfd720d0105deae065b56f8242371bc55c7
BGUNACHI <bhomegagk2002@gmail.com>
2024-03-26 00:38:13 +0530

    Login, Admin integration (admin partly done)

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -4,8 +4,58 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import "./Register.css";
 
 function Register({ handleLoginToggle, isRegisterActive }) {
-    const [alignment, setAlignment] = React.useState('web');
+    const [alignment, setAlignment] = React.useState('customer');
+    const [email, setEmail] = useState('');
+    const [accountPassword, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [bankType, setBankType] = useState('');
 
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        const fetchData = async () => {
+            const apiUrl = (alignment === 'customer') ? `http://localhost:8090/user/customer` : `http://localhost:8090/user/merchant`;
+
+            const params = {
+                userId: 0,
+                name: name,
+                address: address,
+                email: email,
+                phone: phone,
+                type: "string",
+                status: "string",
+                accountPassword: accountPassword,
+                bankType: bankType,
+
+            };
+            try {
+                const response = await fetch(apiUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ params }),
+                });
+                if (response) {
+                    console.log(response);
+                }
+                const data = await response.text();
+                if (response.ok && data === "Login Successful") {
+                    console.log("Registeration successful");
+                } else {
+                    console.log("Enter valid credentials!");
+                }
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+        fetchData();
+    };
     const handleChangeToggle = (event, newAlignment) => {
         setAlignment(newAlignment);
     }
@@ -14,62 +64,72 @@ function Register({ handleLoginToggle, isRegisterActive }) {
     const [formData, setFormData] = useState({
         email: '',
         name: '',
-        phoneNo: '',
+        phone: '',
         address: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        bankType: '',
     })
 
     const [errors, setErrors] = useState({})
 
     const handleChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         setFormData({
-            ...formData, [name] : value
+            ...formData, [name]: value
         })
     }
+
+    const handleCombinedSubmit = (e) => {
+        e.preventDefault();
+
+        handleSubmit(e);
+        handleRegister(e);
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const validationErrors = {}
-        if(!formData.email.trim()) {
-            validationErrors.email = "Email ID is required!"
-        } else if(!/\S+@\S+\.com/.test(formData.email)){
-            validationErrors.email = "Entered email ID is invalid!"
+
+        if (!formData.email || !formData.email.trim()) {
+            validationErrors.email = "Email ID is required!";
+        } else if (!/\S+@\S+\.com/.test(formData.email)) {
+            validationErrors.email = "Entered email ID is invalid!";
         }
 
-        if(!formData.name.trim()){
-            validationErrors.name = "Name is required!"
+        if (!formData.name || !formData.name.trim()) {
+            validationErrors.name = "Name is required!";
         }
 
-        if(!formData.phoneNo.trim()){
-            validationErrors.phoneNo = "Ph no is required!"
+        if (!formData.phone || !formData.phone.trim()) {
+            validationErrors.phone = "Ph no is required!";
         }
 
-        if(!formData.address.trim()){
-            validationErrors.address = "Address is required!"
+        if (!formData.address || !formData.address.trim()) {
+            validationErrors.address = "Address is required!";
         }
 
-        if(!formData.password.trim()) {
-            validationErrors.password = "Password is required!"
-        } else if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/.test(formData.password)){
-            validationErrors.password = "Password format is invalid!"
+        if (!formData.password || !formData.password.trim()) {
+            validationErrors.password = "Password is required!";
+        } else if (!/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\S+$).{8,20}$/.test(formData.password)) {
+            validationErrors.password = "Password format is invalid!";
+            console.log("Enter password satisfying the conditions");
         }
 
-        if(formData.confirmPassword !== formData.password){
-            validationErrors.confirmPassword = "Does not match the password!"
+        if (formData.confirmPassword !== formData.password) {
+            validationErrors.confirmPassword = "Does not match the password!";
         }
 
         setErrors(validationErrors)
 
-        if(Object.keys(validationErrors).length === 0){
-            alert("Registered Successfully")
+        if (Object.keys(validationErrors).length === 0) {
+            console.log("Registeration format is correct");
         }
 
     }
     return (
-        <form onSubmit = {handleSubmit} className="RegisterForm">
-            <fieldset disabled={!isRegisterActive} className ="registerFieldset" >
+        <form onSubmit={handleCombinedSubmit} className="RegisterForm">
+            <fieldset disabled={!isRegisterActive} className="registerFieldset" >
                 <div className="BillingFormHeader">
                     <span className="FormTitle">Registration</span>
                     <span className="FormDetail">Please enter your details</span>
@@ -84,34 +144,43 @@ function Register({ handleLoginToggle, isRegisterActive }) {
                             onChange={handleChangeToggle}
                             aria-label="Platform"
                         >
-                            <ToggleButton value="Customer">Customer</ToggleButton>
-                            <ToggleButton value="Merchant">Merchant</ToggleButton>
+                            <ToggleButton value="customer">Customer</ToggleButton>
+                            <ToggleButton value="merchant">Merchant</ToggleButton>
                         </ToggleButtonGroup>
                     </div>
                     <div className="CardDetails">
                         <label for="email" className="BillingLabel">Email ID</label>
-                        <input name="email" className="BillingTextBox" type = "email" onChange={handleChange}></input>
-                        {errors.email && <span>{errors.email}</span>} <div></div>
+                        <input name="email" className="BillingTextBox" type="email" onChange={(e) => { handleChange(e); setEmail(e.target.value); }} />
+                        <div className="error-message">{errors.email && <div><span>{errors.email}</span><br /></div>} </div>
 
                         <label for="name" className="BillingLabel">Name</label>
-                        <input name="name" className="BillingTextBox" type="text" onChange={handleChange}></input>
-                        {errors.name && <span>{errors.name}</span>} <div></div>
+                        <input name="name" className="BillingTextBox" type="text" onChange={(e) => { handleChange(e); setName(e.target.value); }} />
+                        <div className="error-message">{errors.name && <div><span>{errors.name}</span><br /></div>} </div>
 
-                        <label for="phoneNo" className="BillingLabel">Ph No</label>
-                        <input name="phoneNo" className="BillingTextBox" type="text" onChange={handleChange}></input>
-                        {errors.phoneNo && <span>{errors.phoneNo}</span>} <div></div>
+                        <label for="phone" className="BillingLabel">Phone No</label>
+                        <input name="phone" className="BillingTextBox" type="text" onChange={(e) => { handleChange(e); setPhone(e.target.value); }} />
+                        <div className="error-message">{errors.phone && <div><span>{errors.phone}</span><br /></div>} </div>
 
                         <label for="address" className="BillingLabel">Address</label>
-                        <input name="address" className="BillingTextBox" type="text" onChange={handleChange}></input>
-                        {errors.address && <span>{errors.address}</span>} <div></div>
+                        <input name="address" className="BillingTextBox" type="text" onChange={(e) => { handleChange(e); setAddress(e.target.value); }} />
+                        <div className="error-message">{errors.address && <div><span>{errors.address}</span><br /></div>} </div>
+
+                        <label for="bankType" className="BillingLabel">Bank Type</label>
+                        <select name="bankType" id="bankType" className="BillingTextBox" onClick={(e) => { handleChange(e); setBankType(e.target.value); }} required>
+                            <option value="sbi">State Bank of India</option>
+                            <option value="icici">ICICI Bank</option>
+                            <option value="hdfc">HDFC Bank</option>
+                            <option value="boa">Bank of America</option>
+                            <option value="citi">Citibank</option>
+                        </select>
 
                         <label for="password" className="BillingLabel">Password</label>
-                        <input name="password" className="BillingTextBox" type="password" onChange={handleChange}></input>
-                        {errors.password && <span>{errors.password}</span>} <div></div>
+                        <input name="password" className="BillingTextBox" type="password" onChange={(e) => { handleChange(e); setPassword(e.target.value); }} />
+                        <div className="error-message">{errors.password && <div><span>{errors.password}</span><br /></div>} </div>
 
                         <label for="confirmPassword" className="BillingLabel">Confirm Password</label>
-                        <input name="confirmPassword" className="BillingTextBox" type="password" onChange={handleChange}></input>
-                        {errors.confirmPassword && <span>{errors.confirmPassword}</span>} <div></div>
+                        <input name="confirmPassword" className="BillingTextBox" type="password" />
+                        <div className="error-message">{errors.confirmPassword && <div><span>{errors.confirmPassword}</span><br /></div>} </div>
 
                         <p>Existing User? <button onClick={handleLoginToggle} className="smolLogin">Login</button></p>
                         <button type="submit" className="SignIn">Register</button>
--- src/Components/Login/SignIn/Login.js
+++ src/Components/Login/SignIn/Login.js
@@ -1,15 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import "./Login.css";
 
 
 function Login({ handleLoginToggle, isLoginActive }) {
-    const [alignment, setAlignment] = React.useState('web');
+    const [alignment, setAlignment] = React.useState('user');
+    const [data, setData] = useState([]);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleChangeToggle = (event, newAlignment) => {
-        setAlignment(newAlignment);
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const fetchData = async () => {
+            const apiUrl = `http://localhost:8090/user/Login`;
+
+            const params = new URLSearchParams({
+                email: email,
+                password: password
+            });
+            try {
+                const response = await fetch(`${apiUrl}?${params}`, {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email, password }),   
+                  });
+                  if(response){
+                    console.log(response);
+                  }
+                  const data = await response.text();
+                  if (response.ok && data === "Login Successful") {
+                      console.log("Logging successful");
+                  } else{ 
+                      console.log("Enter valid email and password!");
+                  }
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+        fetchData();
     };
+
 
     const [formData, setFormData] = useState({
         email: '',
@@ -18,35 +55,54 @@ function Login({ handleLoginToggle, isLoginActive }) {
 
     const [errors, setErrors] = useState({})
 
+    const handleChangeToggle = (event, newAlignment) => {
+        setAlignment(newAlignment);
+    };
+
+    
     const handleChange = (e) => {
-        const {name, value} = e.target;
+        console.log(email,password);
+        const { name, value } = e.target;
         setFormData({
-            ...formData, [name] : value
+            ...formData, [name]: value
         })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const validationErrors = {}
-        if(!formData.email.trim()) {
-            validationErrors.email = "Email ID is required!"
-        } else if(!/\S+@\S+\.com/.test(formData.email)){
-            validationErrors.email = "Entered email ID is invalid!"
+        if (!formData.email.trim()) {
+            validationErrors.email = "Email ID is required!";
+            console.log("Email ID is required!");
+        } else if (!/\S+@\S+\.com/.test(formData.email)) {
+            validationErrors.email = "Entered email ID is invalid!";
+            console.log("Entered email ID is invalid!");
         }
 
-        if(!formData.password.trim()) {
-            validationErrors.password = "Password is required!"
+        if (!formData.password.trim()) {
+            validationErrors.password = "Password is required!";
+        } else if (!/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$/.test(formData.password)) {
+            validationErrors.password = "Password format is invalid!";
+            console.log("Enter password satisfying the conditions");
         }
 
         setErrors(validationErrors)
 
-        if(Object.keys(validationErrors).length === 0){
-            alert("Logged in Successfully")
+        if (Object.keys(validationErrors).length === 0) {
+            console.log("Format is fine");
+            ;
         }
-
     }
+
+    const handleCombinedSubmit = (e) => {
+        e.preventDefault();
+
+        handleSubmit(e);
+        handleLogin(e);
+    };
+
     return (
-        <form onSubmit={handleSubmit} className="LoginForm">
+        <form className="LoginForm" onSubmit={handleCombinedSubmit}>
             <fieldset disabled={!isLoginActive} className="loginFieldset" >
                 <div className="BillingFormHeader">
                     <span className="FormTitle">Login</span>
@@ -63,19 +119,19 @@ function Login({ handleLoginToggle, isLoginActive }) {
                             onChange={handleChangeToggle}
                             aria-label="Platform"
                         >
-                            <ToggleButton value="Admin">Admin</ToggleButton>
-                            <ToggleButton value="User">User</ToggleButton>
+                            <ToggleButton value="admin">Admin</ToggleButton>
+                            <ToggleButton value="user">User</ToggleButton>
                         </ToggleButtonGroup>
                     </div>
-                
+
                     <div className="CardDetails">
                         <label for="email" className="BillingLabel">Email ID</label>
-                        <input name="email" className="BillingTextBox" onChange={handleChange}></input>
-                        {errors.email && <span>{errors.email}</span>} <div></div>
+                        <input name="email" className="BillingTextBox" onChange={(e) => { handleChange(e); setEmail(e.target.value); }}></input>
+                        <div className="error-message">{errors.email && <div><span>{errors.email}</span><br/><br/></div>} </div>
 
                         <label for="password" className="BillingLabel">Password</label>
-                        <input name="password" className="BillingTextBox" type="password" onChange={handleChange}></input>
-                        {errors.password && <span>{errors.password}</span>} <div></div>
+                        <input name="password" className="BillingTextBox" type="password" onChange={(e) => { handleChange(e); setPassword(e.target.value); }}></input>
+                        <div className="error-message">{errors.password && <div><span>{errors.password}</span><br/><br/></div>} </div>
 
                         <p>Not an Existing User? <button onClick={handleLoginToggle} className="smolRegister">Register</button></p>
                         <button type="submit" className="SignIn">Sign In</button>
